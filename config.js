b61a34302afd6dff87a3d7febedb984bdd4caaf2
Jirokwbot <165337254+Jirokwbot@users.noreply.github.com>
2024-03-29 06:19:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import { watchFile, unwatchFile } from 'fs' 
+aimport { watchFile, unwatchFile } from 'fs' 
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
@@ -33,7 +33,7 @@ global.lenguajeGB = id  //Idioma de The-LoliBot-MD, Ejemplo: es | en | pt...
 //━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
 global.openai_key = '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' /* Consigue tu ApiKey en este enlace: https://platform.openai.com/account/api-keys */
-global.openai_org_id = 'HITjoN7H8pCwoncEB9e3fSyW' /* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
+global.openai_org_id = 'org-mMpHxN35OMhs6lvfHC6GIztU' /* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
 global.Key360 = ['964f-0c75-7afc']//key de violetics
 global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
